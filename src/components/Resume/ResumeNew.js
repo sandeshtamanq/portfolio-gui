848a2cf0fdd180d882aee61041e83bad93538f71
= <lamasandesh400@gmail.com>
2023-12-14 21:41:12 +0545

    project added

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import "./resume.css";
-import ScrollToTop from "../ScrollToTop/ScrollToTop";
 import resume from "../../Assets/Sandesh-Resume.pdf";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import "./resume.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink = __dirname + "src/Assets/Sandesh-Resume.pdf";
